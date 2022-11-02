2895c0aca61e7f346ce20e87160f242e6717aa82
estefa <estefacdn@gmail.com>
2022-11-01 20:59:24 -0500

    Correcion Formula

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.get('/', (req, res)=>{
         title: 'Suchicharron',
         products:[
             {imagen:'prod1.jpg', name:'Gyozas de Res', description:'Empanadillas rellenas de vegetales y carne de res', price:13000},
-            {imagen:'prod2.jpg', name:'Croquetas de pollo y cilantro', description:'Croquetas de pollo y cilantro', price:13000},
+            {imagen:'prod2.jpg', name:'Croquetas de pollo y cilantro', description:'Croquetas de pollo y cilantro', price:22000},
             {imagen:'prod3.jpg', name:'Pastel de mariscos', description:'Combinación de camarones y calamares apanados en panko acompañado de salsa de tamarindo', price:15000},
             {imagen:'prod4.jpg', name:'Pollo Agridulce en Piña', description:'Pollo en tempura apanado bañado con salsa de piña', price:30000},
             {imagen:'prod5.jpg', name:'Pollo Cherry', description:'Corte de pechuga a la plancha relleno de una salsa a base de pimientos y especias', price:30000},
